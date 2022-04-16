c6bbfc52b425d7e5155629eca936c3f0c36d85c0
Macario12 <arielmacario.11@gmail.com>
2022-04-16 17:15:22 -0600

    docker file suscriber

--- Subscriber/subscriber.go
+++ Subscriber/subscriber.go
@@ -160,7 +160,7 @@ func UltimosJuegos(array []LogStruct) []LogStruct {
 	var UltimosJuegosArray []LogStruct
 	tamanioArray := len(array)
 	if tamanioArray >= 10 {
-		for i := 0; i <= 9; i++ {
+		for i := 1; i <= 9; i++ {
 			UltimosJuegosArray = append(UltimosJuegosArray, array[tamanioArray-i])
 		}
 	}
